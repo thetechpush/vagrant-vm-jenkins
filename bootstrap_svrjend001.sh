28f8f66b11724949d1ae01bf3cd7c8aa396f4356
vagrant <vagrant@svrjend001.example.com>
2020-02-29 08:58:51 +0000

    modified jenkins setup script

--- bootstrap_svrjend001.sh
+++ bootstrap_svrjend001.sh
@@ -3,5 +3,5 @@
 # Initialize Jenkins
 echo "Jenkins starting. Please wait.."
 systemctl start --now jenkins.service > /dev/null 2>&1
-
+echo -e "jenkins\tALL=(ALL)\tNOPASSWD:\tALL" >> /etc/sudoers
 
